2b5d2ce1b3e11e681065f457d0a89b700eaff748
Nate Swartzentruber <nswartzentruber@gmail.com>
2018-07-05 08:08:52 -0400

    Refactor to remove duplicate code

---
+++ LightHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.WebJobs.Host;
+using System;
+
+namespace lifxtriggers
+{
+    public static class LightHelper
+    {
+        public static void UpdateLight(LightSettings settings, string failureMessage, TraceWriter log)
+        {
+            var lightID = Environment.GetEnvironmentVariable("LightId", EnvironmentVariableTarget.Process);
+
+            var provider = new LifxProvider();
+            var lightUpdated = provider.UpdateLight(lightID, settings);
+
+            if (lightUpdated)
+            {
+                log.Info("Successfully updated light.");
+            }
+            else
+            {
+                log.Info("Failed to update light.");
+                var emailProvider = new EmailProvider();
+                var emailSent = emailProvider.SendEmail(failureMessage, "For more details check the Azure function portal.");
+
+                var logMsg = emailSent ? "Message Sent" : "Failed to send message";
+                log.Info(logMsg);
+            }
+        }
+    }
+}
--- TurnOffForNapTimer.cs
+++ TurnOffForNapTimer.cs
@@ -10,29 +10,11 @@ namespace lifxtriggers
         public static void Run([TimerTrigger("0 0 15 * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
-
-            var lightID = Environment.GetEnvironmentVariable("LightId", EnvironmentVariableTarget.Process);
             var settings = new LightSettings
             {
                 Power = "off"
             };
-
-            var provider = new LifxProvider();
-            var lightUpdated = provider.UpdateLight(lightID, settings);
-
-            if (lightUpdated)
-            {
-                log.Info("Successfully updated light.");
-            }
-            else
-            {
-                log.Info("Failed to update light.");
-                var emailProvider = new EmailProvider();
-                var emailSent = emailProvider.SendEmail("Failed to turn off light", "For more details check the Azure function portal.");
-
-                var logMsg = emailSent ? "Message Sent" : "Failed to send message";
-                log.Info(logMsg);
-            }
+            LightHelper.UpdateLight(settings, "Failed to turn off light", log);
         }
     }
 }
